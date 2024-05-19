277ab208ce959b0863e38e4b0fccfa7f0e6697f2
amsamuels <amsamuels21@gmail.com>
2024-05-19 13:35:48 +0100

    delete, read and write to store

--- CompleteStore/store.go
+++ CompleteStore/store.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
 
-func CASPathTransformFunc(key string) string {
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
 
-	return ""
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		Pathname: strings.Join(paths, "/"),
+		Filename: hashStr,
+	}
 }
 
-type PathTransformFunc func(string) string
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	Pathname string
+	Filename string
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.Pathname, p.Filename)
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -26,27 +55,58 @@ func NewStore(StoreOpts StoreOpts) *store {
 		StoreOpts: StoreOpts,
 	}
 }
-func (s *store) writeStream(key string, r io.Reader) string {
-	pathName := s.PathTransformFunc(key)
 
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
-		return err.Error()
+func (s *store) Read(key string) (io.Reader, error) {
+
+	f, err := s.ReadStream(key)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, err
+}
+
+func (s *store) Has(key string) bool {
+	pathKey := s.PathTransformFunc(key)
+	_, err := os.Stat(pathKey.FullPath())
+	if err == fs.ErrNotExist {
+		return false
+	}
+	return true
+}
+
+func (s *store) Delete(key string) error {
+	pathKey := s.PathTransformFunc(key)
+	defer func() {
+		log.Printf("deleted [%s] from disk", pathKey.Filename)
+	}()
+	return os.RemoveAll(pathKey.FullPath())
+}
+func (s *store) ReadStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
+}
+
+func (s *store) Write(key string, r io.Reader) error {
+	pathKey := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathKey.Pathname, os.ModePerm); err != nil {
+		return err
 	}
 
-	filename := "heheje"
-	pathAndFilename := pathName + "/" + filename
-
-	f, err := os.Create(pathAndFilename)
-
+	fullpath := pathKey.FullPath()
+	f, err := os.Create(fullpath)
 	if err != nil {
-		return ""
+		return err
 	}
 
 	c, err := io.Copy(f, r)
 	if err != nil {
-		return ""
+		return err
 	}
-	log.Panicf("written (%d) bytes to disk", c)
-
-	return ""
+	log.Printf("written (%d) bytes to disk", c)
+	return nil
 }
---
+++ CompleteStore/store_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathKey := CASPathTransformFunc(key)
+	expectedFilename := "6804429f74181a63c50c3d81d733a12f14a353ff"
+	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathKey.Pathname != expectedPathName {
+		t.Errorf("have %s want %s", pathKey.Pathname, expectedPathName)
+	}
+
+	if pathKey.Filename != expectedFilename {
+		t.Errorf("have %s want %s", pathKey.Filename, expectedFilename)
+	}
+}
+
+func TestStoreDeletKey(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+
+	store := NewStore(opts)
+	key := "myspecialpicture"
+	//data := []byte("some jpg byte")
+
+	// if err := store.Write(key, bytes.NewReader(data)); err != nil {
+	// 	t.Error(err)
+	// }
+	if err := store.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestStore(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+
+	store := NewStore(opts)
+	key := "myspecialpicture"
+	data := []byte("some jpg byte")
+
+	if err := store.Write(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	r, err := store.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+
+	if string(b) != string(data) {
+		t.Errorf("want %s have %s", data, b)
+	}
+
+}
